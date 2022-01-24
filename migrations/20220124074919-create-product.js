487f507288c7841997a94ab4ad069792973d2d51
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2022-01-24 14:52:07 +0700

    new lagi

---
+++ migrations/20220124074919-create-product.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      code_product: {
+        type: Sequelize.STRING
+      },
+      product: {
+        type: Sequelize.STRING
+      },
+      total: {
+        type: Sequelize.INTEGER
+      },
+      unit_price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('products');
+  }
+};
